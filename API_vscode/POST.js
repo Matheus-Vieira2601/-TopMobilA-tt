32331c81d515229e06311e8b5d031feead9dacaa
JOAO VITOR MAIA <u21240@academico.cotuca.unicamp.br>
2022-10-07 12:07:10 -0300

    api

---
+++ API_vscode/POST.js
@@ -0,0 +1,17 @@
+
+const data = { username: "Victor Jordan" };
+
+fetch("https://backefront.com.br/api/users", {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+  },
+  body: JSON.stringify(data),
+})
+  .then((response) => response.json())
+  .then((data) => {
+    console.log("Success:", data);
+  })
+  .catch((error) => {
+    console.error("Error:", error);
+  });
